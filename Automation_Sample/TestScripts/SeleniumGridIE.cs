b73c8c44739bd29987123c83faae2a68d981812e
arunksahoo <arun.sahoo01@gmail.com>
2019-06-25 15:53:54 +0530

    first commit

---
+++ Automation_Sample/TestScripts/SeleniumGridIE.cs
@@ -0,0 +1,77 @@
+﻿using AventStack.ExtentReports;
+using Automation_Sample.Utilites;
+using log4net;
+using log4net.Config;
+using NUnit.Framework;
+using System;
+using Automation_Sample.ApplicationPage;
+using OpenQA.Selenium.Remote;
+
+namespace Automation_Sample.TestScripts
+{
+    class SeleniumGridIE
+    {
+        EReports report = new EReports();
+        protected ILog logger = LogManager.GetLogger(typeof(SeleniumGridIE));
+        public static dynamic validationData = ReadingFile.RdWrJsonFile("ValidationData");
+        [OneTimeSetUp]
+        public void File()
+        {
+
+            report.StartReports();
+        }
+        [SetUp]
+        public void Intilize()
+        {
+            DesiredCapabilities Capabilities = new DesiredCapabilities();
+            Capabilities.SetCapability(CapabilityType.BrowserName, "internet explorer");
+
+            //Hub URL
+            String huburl = "http://172.16.48.53:4547/wd/hub";
+
+            // Create driver with hub address and capability
+            ParallelTestExecution.Driver = new RemoteWebDriver(new Uri(huburl), Capabilities);
+
+            string URL = ReadingFile.RdWrJsonFile("ApplicationSetUp").Application_URL;
+            report.BeforeTest();
+            logger.Debug("Lunched Browser");
+            ParallelTestExecution.Driver.Manage().Window.Maximize();
+            ParallelTestExecution.Driver.Navigate().GoToUrl(URL);
+            CutomizedBasePage.WaitonPage(5);
+            logger.Info("Navigate to UI");
+        }
+
+        // Create a Portfolio and Then Delete the Portfolio functinality
+        [Test]
+        public void Applicationflow()
+        {
+            HomePage.ClickOnContact();
+            var contactNumber = ContactPage.HyderbadConatctNumbergetText().Text;
+            String ActualContact = contactNumber.ToString().TrimStart();
+            string ExpectedContact = validationData.Hyderbad_ConatctNumber_Text;
+            Assert.AreEqual(ActualContact, ExpectedContact);
+
+            HomePage.ClickOnWorksButton();
+            var filterMenuName = WorksPage.Filtertitle_getText().Text;
+            String ActualfilterMenuName = filterMenuName.ToString().TrimStart();
+            string ExpectedfilterMenuName = validationData.WorksPage_FilterMenu_Text;
+            Assert.AreEqual(ActualfilterMenuName, ExpectedfilterMenuName);
+
+
+        }
+        [TearDown]
+        public void CloseBrowser()
+        {
+
+            ParallelTestExecution.Driver.Quit();
+            logger.Debug("Browser is closed");
+
+        }
+        [OneTimeTearDown]
+        public void EndReport()
+        {
+            report.AfterClass();
+        }
+    }
+}
+
